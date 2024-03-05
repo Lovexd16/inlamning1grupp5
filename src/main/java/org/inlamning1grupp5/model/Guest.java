ca46de4bb3996b34569d7d81e661e9567d71080a
David Hankin <d_hankin@outlook.com>
2024-03-05 12:59:13 +0100

    guest class

---
+++ src/main/java/org/inlamning1grupp5/model/Guest.java
@@ -0,0 +1,76 @@
+package org.inlamning1grupp5.model;
+
+public class Guest {
+    
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String address1;
+
+    private String address2;
+
+    private String postnumber;
+
+    private String city;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getPostnumber() {
+        return postnumber;
+    }
+
+    public void setPostnumber(String postnumber) {
+        this.postnumber = postnumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    
+}
